edf581658bd7cea10bfde8393ede7e367c392f6a
WatsonWaswa <92554023+WatsonWaswa@users.noreply.github.com>
2025-03-22 12:19:22 +0300

    Update watson.py

--- watson.py
+++ watson.py
@@ -14,3 +14,11 @@ print("Week 3 - 03/10/2025")
 print("Introduced to HTML")
 print("Learnt Python")
 print("Learnt Databases")
+print("Learnt about Startup Building for developers")
+
+
+print("Week 4")
+print(" Learnt web development")
+print("Learnt Python Navigation")
+print("Learnt Databases - SQL practicle")
+print("Learnt about Startup Building for developers")
